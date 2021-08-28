3b93fddac9cda2aafe26fa6bdc24be9f0c19055c
Daniel Pérez <plupyt_dany@hotmail.com>
2021-08-28 12:25:44 -0500

    boilerplate listo

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Article finder</title>
+    <style type="text/css">@import url("styles/index.css");</style>
+</head>
+<body>
+    <h1 class="pageTitle">Article finder</h1>
+    <div class="search">
+        <form class="search_form" action="">
+            <label for="inputSearch"></label>
+            <input class= "search_form-input" name="inputSearch" type="text">
+            <button class="search_form-button" type="submit">Buscar</button>
+        </form>
+    </div>
+    <div class="results">
+        <p>Bueno, aquí irían los resultados</p>
+    </div>
+
+</body>
+</html>
